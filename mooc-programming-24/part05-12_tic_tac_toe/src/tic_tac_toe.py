817538df4f3982c01a1ba6015038dbe5a931de12
Tiah Rami <elolamrami@gmail.com>
2024-09-03 10:57:13 +0100

    new exercises on 2D list.

--- mooc-programming-24/part05-12_tic_tac_toe/src/tic_tac_toe.py
+++ mooc-programming-24/part05-12_tic_tac_toe/src/tic_tac_toe.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def play_turn(game_board: list, x: int, y: int, piece: str) -> bool:
+    new_board = []
+    if (y < 0  or y < 0) or (y >= len(game_board) or x >= len(game_board[y])):
+        return False
+
+    if game_board[y][x] == "":
+        game_board[y][x] = piece
+        return True
+    else:
+        return False
+
+if __name__ == "__main__":
+    game_board = [["", "", ""], ["", "", ""], ["", "", ""]]
+    print(play_turn(game_board, 2, 0, "X"))
+    print(game_board)
--- mooc-programming-24/part05-13_transpose_matrix/src/transpose_matrix.py
+++ mooc-programming-24/part05-13_transpose_matrix/src/transpose_matrix.py
@@ -1,1 +1,20 @@
 # Write your solution here
+def transpose(matrix: list):
+    new_matrix = []
+    i = 0
+    for row in range(len(matrix)):
+        for col in range(len(matrix)):
+            new_matrix.append(matrix[col][row])
+
+    for row in range(len(matrix)):
+        for col in range(len(matrix)):
+            print(new_matrix[i], end = " ")
+            i += 1
+        print()
+
+
+if __name__ == "__main__":
+    matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    print(matrix)
+    transpose(matrix)
+    print(matrix)
